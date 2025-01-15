b2c0546a452224beef67411acdcc2f187f38636a
Dave Carr <davecarr1024@gmail.com>
2025-01-15 16:24:24 -0500

    regexes

--- Shype/Shype/Core/Regex/Regex.cs
+++ Shype/Shype/Core/Regex/Regex.cs
@@ -5,6 +5,9 @@ namespace Shype.Core.Regex;
 
 public abstract record Regex : Errors.Errorable<Regex>
 {
+    public (State state, Result result) Apply(string input, Chars.Position? position = null)
+        => Apply(new(input, position));
+
     public abstract (State state, Result result) Apply(State state);
 
     private abstract record UnaryRegex(Regex Child) : Regex
@@ -26,7 +29,7 @@ public abstract record Regex : Errors.Errorable<Regex>
                     (state, Result child_result) = ApplyChild(state);
                     result += child_result;
                 }
-                catch (Error)
+                catch (Errors.Error)
                 {
                     return (state, result);
                 }
@@ -36,29 +39,71 @@ public abstract record Regex : Errors.Errorable<Regex>
 
     public Regex ZeroOrMore() => new ZeroOrMoreRegex(this);
 
-    private abstract record HeadRegex : Regex
+    private record ZeroOrOneRegex(Regex Child) : UnaryRegex(Child)
     {
-        protected abstract Chars.Char Apply(Chars.Char head);
+        public override string ToString() => $"{Child}?";
 
         public override (State state, Result result) Apply(State state)
-            => (
-                state.Tail(),
-                new Result(
-                    [
-                        Try(
-                            () => Apply(state.Head()),
-                            $"error while processing head {state.Head()}"
-                        )
-                    ]
-                )
-            );
+        {
+            try
+            {
+                return ApplyChild(state);
+            }
+            catch (Errors.Error)
+            {
+                return (state, new());
+            }
+        }
+    }
+
+    public Regex ZeroOrOne() => new ZeroOrOneRegex(this);
+
+    private record OneOrMoreRegex(Regex Child) : UnaryRegex(Child)
+    {
+        public override string ToString() => $"{Child}+";
+
+        public override (State state, Result result) Apply(State state)
+        {
+            Result result = new();
+            (state, Result child_result) = ApplyChild(state);
+            result += child_result;
+            while (true)
+            {
+                try
+                {
+                    (state, child_result) = ApplyChild(state);
+                    result += child_result;
+                }
+                catch (Errors.Error)
+                {
+                    return (state, result);
+                }
+            }
+        }
+    }
+
+    public Regex OneOrMore() => new OneOrMoreRegex(this);
+
+    private abstract record HeadRegex : Regex
+    {
+        public abstract Chars.Char Apply(Chars.Char head);
+
+        protected Chars.Char Head(State state) => Try(state.Head, "empty state");
+
+        public override (State state, Result result) Apply(State state)
+        {
+            Chars.Char head = Head(state);
+            return (state.Tail(), new(Try(() => Apply(head), $"error while processing head {head}")));
+        }
+
+        public override Regex Not() => new NotRegex(this);
     }
 
     private record LiteralRegex(char Value) : HeadRegex
     {
         public override string ToString() => Value.ToString();
 
-        protected override Chars.Char Apply(Chars.Char head)
+        public override Chars.Char Apply(Chars.Char head)
         {
             if (head.Value != Value)
             {
@@ -69,6 +114,68 @@ public abstract record Regex : Errors.Errorable<Regex>
     }
 
     public static Regex Literal(char value) => new LiteralRegex(value);
+
+    private record AnyRegex : HeadRegex
+    {
+        public override string ToString() => ".";
+
+        public override Chars.Char Apply(Chars.Char head) => head;
+    }
+
+    public static Regex Any() => new AnyRegex();
+
+    private record RangeRegex(char Min, char Max) : HeadRegex
+    {
+        public override string ToString() => $"[{Min}-{Max}]";
+
+        public override Chars.Char Apply(Chars.Char head)
+        {
+            if (head.Value < Min || head.Value > Max)
+            {
+                throw CreateError($"expected head in range {Min}-{Max} got {head.Value}");
+            }
+            return head;
+        }
+    }
+
+    public static Regex Range(char min, char max) => new RangeRegex(min, max);
+
+    private record ClassRegex(IImmutableSet<char> Values, string? Display) : HeadRegex
+    {
+        public override string ToString() => Display ?? $"({string.Concat(Values)})";
+
+        public override Chars.Char Apply(Chars.Char head)
+            => Values.Contains(head.Value) ? head : throw CreateError($"expected head in set {Values} got {head.Value}");
+    }
+
+    public static Regex Class(IImmutableSet<char> values, string? display = null) => new ClassRegex(values, display);
+
+    public static Regex Whitespace() => Class(" \t\n".ToImmutableHashSet(), "\\w");
+
+    public static Regex Digits() => Class("0123456789".ToImmutableHashSet(), "\\d");
+
+    private record NotRegex(HeadRegex Child) : HeadRegex
+    {
+        public override string ToString() => $"^{Child}";
+
+        public override (State state, Result result) Apply(State state)
+        {
+            Chars.Char head = Head(state), child_result;
+            try
+            {
+                child_result = Child.Apply(head);
+            }
+            catch (Errors.Error)
+            {
+                return (state.Tail(), new(head));
+            }
+            throw CreateError($"child {Child} expected to not match but matched {child_result}");
+        }
+
+        public override Chars.Char Apply(Chars.Char head) => throw new NotImplementedException();
+    }
+
+    public virtual Regex Not() => throw new NotImplementedException($"non-head regex {this} doesn't support not");
 
     private abstract record NaryRegex(IImmutableList<Regex> Children) : Regex, IEnumerable<Regex>
     {
--- Shype/ShypeTest/Core/Regex/RegexTest.cs
+++ Shype/ShypeTest/Core/Regex/RegexTest.cs
@@ -6,10 +6,10 @@ public class RegexTest
     [TestMethod]
     public void TestLiteral()
     {
-        Assert.ThrowsException<Regex.Error>(() => Regex.Literal('a').Apply(new State("b")));
+        Assert.ThrowsException<Regex.Error>(() => Regex.Literal('a').Apply(new("b")));
         Assert.AreEqual(
             (new State(), new Result("a")),
-            Regex.Literal('a').Apply(new State("a"))
+            Regex.Literal('a').Apply(new("a"))
         );
     }
 
@@ -19,27 +19,73 @@ public class RegexTest
         Regex regex = Regex.Literal('a').ZeroOrMore();
         Assert.AreEqual(
             (new State(), new Result()),
-            regex.Apply(new State())
+            regex.Apply(new())
         );
         Assert.AreEqual(
             (new State("b"), new Result()),
-            Regex.Literal('a').ZeroOrMore().Apply(new State("b"))
+            Regex.Literal('a').ZeroOrMore().Apply(new("b"))
         );
         Assert.AreEqual(
             (new State("b", new(0, 1)), new Result("a")),
-            Regex.Literal('a').ZeroOrMore().Apply(new State("ab"))
+            Regex.Literal('a').ZeroOrMore().Apply(new("ab"))
         );
         Assert.AreEqual(
             (new State("b", new(0, 2)), new Result("aa")),
-            Regex.Literal('a').ZeroOrMore().Apply(new State("aab"))
+            Regex.Literal('a').ZeroOrMore().Apply(new("aab"))
         );
         Assert.AreEqual(
             (new State(), new Result("a")),
-            Regex.Literal('a').ZeroOrMore().Apply(new State("a"))
+            Regex.Literal('a').ZeroOrMore().Apply(new("a"))
         );
         Assert.AreEqual(
             (new State(), new Result("aa")),
-            Regex.Literal('a').ZeroOrMore().Apply(new State("aa"))
+            Regex.Literal('a').ZeroOrMore().Apply(new("aa"))
+        );
+    }
+
+    [TestMethod]
+    public void TestOneOrMore()
+    {
+        Regex regex = Regex.Literal('a').OneOrMore();
+        Assert.ThrowsException<Regex.Error>(() => regex.Apply(new()));
+        Assert.ThrowsException<Regex.Error>(() => regex.Apply(new("b")));
+        Assert.AreEqual(
+            (new State("b", new(0, 1)), new Result("a")),
+            Regex.Literal('a').OneOrMore().Apply(new("ab"))
+        );
+        Assert.AreEqual(
+            (new State("b", new(0, 2)), new Result("aa")),
+            Regex.Literal('a').OneOrMore().Apply(new("aab"))
+        );
+        Assert.AreEqual(
+            (new State(), new Result("a")),
+            Regex.Literal('a').OneOrMore().Apply(new("a"))
+        );
+        Assert.AreEqual(
+            (new State(), new Result("aa")),
+            Regex.Literal('a').OneOrMore().Apply(new("aa"))
+        );
+    }
+
+    [TestMethod]
+    public void TestZeroOrOne()
+    {
+        Regex regex = Regex.Literal('a').ZeroOrOne();
+        Assert.AreEqual(
+            (new State(), new Result()),
+            regex.Apply(new())
+        );
+        Assert.AreEqual(
+            (new State("b"), new Result()),
+            Regex.Literal('a').ZeroOrMore().Apply(new("b"))
+        );
+        Assert.AreEqual(
+            (new State("b", new(0, 1)), new Result("a")),
+            Regex.Literal('a').ZeroOrOne().Apply(new("ab"))
+        );
+        Assert.AreEqual(
+            (new State(), new Result("a")),
+            Regex.Literal('a').ZeroOrOne().Apply(new("a"))
         );
     }
 
@@ -49,23 +95,92 @@ public class RegexTest
         Regex regex = Regex.Literal('a') & Regex.Literal('b');
         Assert.AreEqual(
             (new State(), new Result("ab")),
-            regex.Apply(new State("ab"))
+            regex.Apply("ab")
         );
         Assert.AreEqual(
             (new State("c", new(0, 2)), new Result("ab")),
-            regex.Apply(new State("abc"))
+            regex.Apply(new("abc"))
         );
         Assert.ThrowsException<Regex.Error>(
-            () => regex.Apply(new State("a"))
+            () => regex.Apply("a")
         );
         Assert.ThrowsException<Regex.Error>(
-            () => regex.Apply(new State("c"))
+            () => regex.Apply(new("c"))
         );
         Assert.ThrowsException<Regex.Error>(
-            () => regex.Apply(new State("b"))
+            () => regex.Apply(new("b"))
         );
         Assert.ThrowsException<Regex.Error>(
-            () => regex.Apply(new State("c"))
+            () => regex.Apply(new("c"))
+        );
+    }
+
+    [TestMethod]
+    public void TestAny()
+    {
+        Regex regex = Regex.Any();
+        Assert.ThrowsException<Regex.Error>(() => regex.Apply(new()));
+        Assert.AreEqual(
+            (new State(), new Result("a")),
+            regex.Apply("a")
+        );
+        Assert.AreEqual(
+            (new State("b", new(0, 1)), new Result("a")),
+            regex.Apply("ab")
+        );
+    }
+
+    [TestMethod]
+    public void TestRange()
+    {
+        Regex regex = Regex.Range('a', 'z');
+        Assert.ThrowsException<Regex.Error>(() => regex.Apply(""));
+        Assert.ThrowsException<Regex.Error>(() => regex.Apply("A"));
+        Assert.AreEqual(
+            (new State(), new Result("a")),
+            regex.Apply("a")
+        );
+        Assert.AreEqual(
+            (new State(), new Result("b")),
+            regex.Apply("b")
+        );
+        Assert.AreEqual(
+            (new State(), new Result("z")),
+            regex.Apply("z")
+        );
+        Assert.AreEqual(
+            (new State("b", new(0, 1)), new Result("a")),
+            regex.Apply("ab")
+        );
+    }
+
+    [TestMethod]
+    public void TestClass()
+    {
+        Regex regex = Regex.Digits();
+        Assert.ThrowsException<Regex.Error>(() => regex.Apply(""));
+        Assert.ThrowsException<Regex.Error>(() => regex.Apply("a"));
+        Assert.AreEqual(
+            (new State(), new Result("0")),
+            regex.Apply("0")
+        );
+        Assert.AreEqual(
+            (new State("b", new(0, 1)), new Result("0")),
+            regex.Apply("0b")
+        );
+        Assert.AreEqual("\\d", regex.ToString());
+    }
+
+    [TestMethod]
+    public void TestNot()
+    {
+        Assert.ThrowsException<NotImplementedException>((Regex.Literal('a') & Regex.Literal('b')).Not);
+        Regex regex = Regex.Literal('a').Not();
+        Assert.ThrowsException<Regex.Error>(() => regex.Apply(""));
+        Assert.ThrowsException<Regex.Error>(() => regex.Apply("a"));
+        Assert.AreEqual(
+            (new State(), new Result("b")),
+            regex.Apply("b")
         );
     }
 }
